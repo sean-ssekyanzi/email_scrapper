c1c877591f8895580dc547288e33ffa204081e5c
sean-ssekyanzi <msean481@gmail.com>
2024-04-11 10:37:12 +0300

    chore:renaming files and cleaning up the url grabber

--- caller.py
+++
@@ -1,6 +0,0 @@
-from main import company_reader,grabs_url
- 
-
-# company_reader("file.txt")
-
-grabs_url(company_reader("file.txt"))
---
+++ functions.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+def company_reader(filename:str)->list:
+    print(f" reading companies in {filename}")
+    companies = []
+    try:
+        with open(filename,'r') as file:
+            for item in file:
+                strip = item.rstrip('\n')
+                companies.append(strip)
+            print(companies) 
+    except Exception as e:
+        print("An error occured:", str(e))
+    return companies
+        
+
+def grabs_url(company_reader)->str:
+    print(f"accessing {company_reader}")
+    list = company_reader
+    for url in list:
+        print(url)
+        page = requests.get(f'https://www.google.com/search?q={url}') 
+        soup = BeautifulSoup(page.text,'html.parser')
+        for link in soup.find_all('a'):
+            links = (link.get('href'))
+            print(links)
+    return links
+
+
+
+if __name__ == "main":
+    print("Initialising")
+    
+    
+    
